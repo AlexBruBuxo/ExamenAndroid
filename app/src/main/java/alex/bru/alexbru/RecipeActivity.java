fbef017e2f35e2460bcdc8af98f399fdafed732a
AlexBruBuxo <brubuxo@gmail.com>
2018-11-06 11:52:10 +0100

    Pregunta 2 i 3

--- app/src/main/java/alex/bru/alexbru/RecipeActivity.java
+++ app/src/main/java/alex/bru/alexbru/RecipeActivity.java
@@ -2,7 +2,9 @@ package alex.bru.alexbru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RecipeActivity extends AppCompatActivity {
@@ -12,18 +14,27 @@ public class RecipeActivity extends AppCompatActivity {
     private List<String> ingredients;
     private String description = "Cortar papatas, freírlas suevamente...";
 
+    private TextView name_view, minutes_view, ingredients_view, description_view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
+        ingredients = new ArrayList<>();
         ingredients.add("Patatas");
         ingredients.add("Huevos");
-        
+
         recipe = new Recipe(
                 "Tortilla de Patatas",
                 45,
                 ingredients,
                 description);
+
+        name_view = findViewById(R.id.name_view);
+        minutes_view = findViewById(R.id.minutes_view);
+        description_view = findViewById(R.id.description_view);
+        ingredients_view = findViewById(R.id.ingredients_view);
+
     }
 }
