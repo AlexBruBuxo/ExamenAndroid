094214e53aebf72b09e62b5003bbbde885446bff
AlexBruBuxo <brubuxo@gmail.com>
2018-11-06 12:07:16 +0100

    Pregunta 5

---
+++ app/src/main/java/alex/bru/alexbru/NewIngredientActivity.java
@@ -0,0 +1,13 @@
+package alex.bru.alexbru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewIngredientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_ingredient);
+    }
+}
